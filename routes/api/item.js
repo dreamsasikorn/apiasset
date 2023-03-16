e758d074db021ee76959a0453f5ec44417a27880
dreamsasikorn <dreamsasikorn@gmail.com>
2023-03-16 19:56:12 +0700

    route

--- routes/api/item.js
+++ routes/api/item.js
@@ -9,7 +9,8 @@ router.use((req, res, next) => {
 })
 
 router.get('/', (req, res) => {
-    let sql = "select * from ietlasset.items"
+    const items = []
+    let sql = "select i.id,i.name_item,i.itemcode,i.amount, it.type_name,i.created_at from ietlasset.items i left join itemtype it on it.id = i.item_type_id"
     conn.query(sql, function (err, result) {
         if (err) {
             res.setHeader('Content-Type', 'application/json');
@@ -20,11 +21,21 @@ router.get('/', (req, res) => {
             }
             res.send(data)
         }
+        result.forEach(element => {
+            items.push({
+                "id": element.id,
+                "name_item": element.name_item,
+                "itemcode": element.itemcode,
+                "amount": element.amount,
+                "type_name": element.type_name,
+                "created_at": (element.created_at).toLocaleString()
+            })
+        });
         res.setHeader('Content-Type', 'application/json');
         data = {
             'message': 'success',
             'numrow': result.length,
-            'data': result
+            'data': items
         }
 
         res.status(200).send(data)
@@ -34,4 +45,7 @@ router.get('/', (req, res) => {
     // res.send('api')
 });
 
+router.post('/create', (req, res) => {
+
+})
 module.exports = router
